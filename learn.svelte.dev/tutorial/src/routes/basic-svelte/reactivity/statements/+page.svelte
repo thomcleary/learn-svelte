a4b8773ececf3c4ab9cb20e65872b8bf8fe5624c
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-27 16:01:24 +0800

    svelte.dev tutorial: Part 1 / Reactivity / Statements

---
+++ learn.svelte.dev/tutorial/src/routes/basic-svelte/reactivity/statements/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
+
+	$: console.log(`The count is ${count}`);
+
+	$: {
+		console.log(`The count doubled is ${count * 2}`);
+		console.log('This will also be logged whenever the count changes');
+	}
+
+	$: if (count >= 10) {
+		alert('count is dangerously high!');
+		count = 0;
+	}
+</script>
+
+<button on:click={handleClick}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
