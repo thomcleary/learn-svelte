a4ffcdedb1df783590ef98d8ba38526943c52a1f
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-27 15:52:36 +0800

    svelte.dev tutorial: Part 1 / Reactivity / Assignments

---
+++ learn.svelte.dev/tutorial/src/routes/basic-svelte/reactivity/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Reactivity</h1>
+
+<ol>
+	<li><a href="reactivity/assignments">Assignments</a></li>
+	<li><a href="reactivity/declarations">Declarations</a></li>
+	<li><a href="reactivity/statements">Statements</a></li>
+	<li><a href="reactivity/update-arrays-and-objects">Updating arrays and objects</a></li>
+</ol>
---
+++ learn.svelte.dev/tutorial/src/routes/basic-svelte/reactivity/assignments/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	let count = 0;
+
+	const increment = () => count++;
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
