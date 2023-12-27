d2a02160b747d332d46656e792c242b6bae02f62
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-27 16:21:10 +0800

    svelte.dev tutorial: Part 1 / Props / Declaring props

---
+++ learn.svelte.dev/tutorial/src/routes/basic-svelte/props/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Props</h1>
+
+<ol>
+	<li><a href="props/declaring-props">Declaring props</a></li>
+	<li><a href="props/default-values">Default values</a></li>
+	<li><a href="props/spread-props">Spread props</a></li>
+</ol>
