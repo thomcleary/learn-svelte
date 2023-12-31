a84eb29feedb99020df78969ff04ccb3e8e0a142
Thomas Cleary <thomasclearydev@gmail.com>
2023-12-31 19:27:42 +0800

    svelte.dev tutorial: Part 2 / Advanced bindings

---
+++ svelte.dev/tutorial/src/routes/advanced-svelte/advanced-bindings/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Advanced bindings</h1>
+
+<ol>
+	<li><a href="advanced-bindings/contenteditable-bindings">Contenteditable bindings</a></li>
+	<li><a href="advanced-bindings/each-block-bindings">Each block bindings</a></li>
+	<li><a href="advanced-bindings/media-elements">Media elements</a></li>
+	<li><a href="advanced-bindings/dimensions">Dimensions</a></li>
+	<li><a href="advanced-bindings/this">This</a></li>
+	<li><a href="advanced-bindings/component-bindings">Component bindings</a></li>
+	<li>
+		<a href="advanced-bindings/binding-to-component-instances">Binding to component instances</a>
+	</li>
+</ol>
---
+++ svelte.dev/tutorial/src/routes/advanced-svelte/advanced-bindings/contenteditable-bindings/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	let html = '<span>Write some <b>innerHTML!</b></span>';
+	let textContent = '<span>Write some <b>textContent!</b></span>';
+</script>
+
+<div bind:innerHTML={html} contenteditable />
+<div bind:textContent contenteditable />
+
+<pre>innerHTML: {@html html}</pre>
+<pre>textContent: {@html textContent}</pre>
+
+<style>
+	[contenteditable] {
+		padding: 0.5em;
+		border: 1px solid #eee;
+		border-radius: 4px;
+	}
+</style>
