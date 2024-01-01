043ca71afa5c07081dcfaf9b332a1ac4a73e419f
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-01 17:21:42 +0800

    svelte.dev tutorial: Part 2 / Miscellaneous

---
+++ svelte.dev/tutorial/src/routes/advanced-svelte/miscellaneous/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Miscellaneous</h1>
+
+<ol>
+	<li><a href="miscellaneous/the-@debug-tag">The @debug tag</a></li>
+</ol>
---
+++ svelte.dev/tutorial/src/routes/advanced-svelte/miscellaneous/the-@debug-tag/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	let user = {
+		firstname: 'Ada',
+		lastname: 'Lovelace'
+	};
+</script>
+
+{@debug user}
+
+<label>
+	<input bind:value={user.firstname} />
+	first name
+</label>
+
+<label>
+	<input bind:value={user.lastname} />
+	last name
+</label>
+
+{(console.log(user), '')}
+
+<h1>Hello {user.firstname}!</h1>
