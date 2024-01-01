9d4a97c79e22c51b12f14fee309443250a3d461f
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-01 13:19:09 +0800

    svelte.dev tutorial: Part 2 / Component composition

---
+++ svelte.dev/tutorial/src/routes/advanced-svelte/component-composition/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Component composition</h1>
+
+<ol>
+	<li><a href="component-composition/slots">Slots</a></li>
+	<li><a href="component-composition/named-slots">Named slots</a></li>
+	<li><a href="component-composition/slot-fallbacks">Slot fallbacks</a></li>
+	<li><a href="component-composition/slot-props">Slot props</a></li>
+	<li><a href="component-composition/checking-for-slot-content">Checking for slot content</a></li>
+</ol>
---
+++ svelte.dev/tutorial/src/routes/advanced-svelte/component-composition/slots/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Card from './Card.svelte';
+</script>
+
+<main>
+	<Card>
+		<!-- content goes here -->
+		<span>Patrick BATEMAN</span>
+		<span>Vice President</span>
+	</Card>
+</main>
+
+<style>
+	main {
+		display: grid;
+		place-items: center;
+		height: 100%;
+		background: url(./wood.svg);
+	}
+</style>
