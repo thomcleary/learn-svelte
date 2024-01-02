1e7e431cfaecbf8c26bb70f792ce701807eb837a
Thomas Cleary <thomasclearydev@gmail.com>
2024-01-02 13:29:24 +0800

    svelte.dev tutorial: Part 3 / Shared modules

---
+++ svelte.dev/tutorial/src/routes/basic-sveltekit/shared-modules/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Shared modules</h1>
+
+<ol>
+	<li><a href="shared-modules/the-$lib-alias">The $lib alias</a></li>
+</ol>
---
+++ svelte.dev/tutorial/src/routes/basic-sveltekit/shared-modules/the-$lib-alias/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	const href = '/basic-sveltekit/shared-modules/the-$lib-alias';
+</script>
+
+<nav>
+	<a {href}>home</a>
+	<a href={`${href}/a/deeply/nested/route`}>a deeply nested route</a>
+</nav>
+
+<slot />
